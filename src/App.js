be4663e5ee3f542bff15b8c5e10f0681a5533d6c
A <alzemamia@gmail.com>
2024-07-15 00:52:33 +0300

    frontend setup done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Routes, useNavigate } from "react-router-dom";
+
+import UserContext from "./context/UserContext";
+import { useEffect, useState } from "react";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
-  return <div className="App">Hello capstone</div>;
+  const [user, setUser] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setUser(true);
+    }
+  }, []);
+
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <UserContext.Provider value={[user, setUser]}>
+        <Routes></Routes>
+      </UserContext.Provider>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Cormorant: ["Cormorant Garamond", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
