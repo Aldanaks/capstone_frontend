9cf26880b8b0706fa4d4ae4539722c981516edcc
Azmenezi <132600792+Azmenezi@users.noreply.github.com>
2024-07-30 15:12:25 +0300

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // import { getToken } from "./storage";
 
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://fluid-backend.turguta.com";
 
 const instance = axios.create({
   baseURL: BASE_URL,
